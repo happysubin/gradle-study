9dce97a55baaa22612b832925ecf9b97575bc465
happysubin <asb0711@gmail.com>
2022-12-14 01:29:13 +0900

    first commit

---
+++ src/main/java/study/gradle/GradleStudyApplication.java
@@ -0,0 +1,13 @@
+package study.gradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleStudyApplication.class, args);
+	}
+
+}
